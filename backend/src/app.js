24bcc880f7de4c0128131f14f62f582d5d024d62
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-08-11 13:25:08 -0300

    Initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+// não remova esse endpoint, é para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.json({ status: 'Store Manager UP!' });
+});
+
+module.exports = app;
