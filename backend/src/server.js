ba798eaf8979bc91b2770d38a44d20ed820fba1f
Lionel Miranda <bibliothecalionel@gmail.com>
2023-08-20 16:09:56 -0400

    iniciando

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`Backend do Store Manager escutando na porta ${PORT}`);
+  console.log(`Backend do Store Manager escutando na porta: ${PORT}`);
 });
